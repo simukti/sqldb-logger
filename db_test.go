94c05bfe4b52718af16409eb2a39e5a44738a7be
Sarjono Mukti Aji <me@simukti.net>
2019-11-09 22:26:52 +0400

    remove DATA-DOG/go-sqlmock

--- db_test.go
+++ db_test.go
@@ -6,73 +6,94 @@ import (
 	"context"
 	"database/sql"
 	"database/sql/driver"
+	"encoding/json"
 	"testing"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestConnector_Connect(t *testing.T) {
-	cfg := &config{}
-	setDefaultConfig(cfg)
-	dbmock, _, _ := sqlmock.New()
-	con := &connector{
-		dsn:    "sqlmock_db_0", // this 0 to use sql mock counter
-		driver: dbmock.Driver(),
-		logger: &logger{
-			logger: &bufferTestLogger{},
-			cfg:    cfg,
-		},
-	}
-
-	c, err := con.Connect(context.TODO())
-	assert.NoError(t, err)
-	_, ok := c.(driver.Conn)
-	assert.True(t, ok)
-	_ = dbmock.Close()
-}
-
-func TestConnector_ConnectError(t *testing.T) {
-	cfg := &config{}
-	setDefaultConfig(cfg)
-	dbmock, _, _ := sqlmock.New()
-	con := &connector{
-		dsn:    "sqlmock_err_always",
-		driver: dbmock.Driver(),
-		logger: &logger{
-			logger: &bufferTestLogger{},
-			cfg:    cfg,
-		},
-	}
-
-	_, err := con.Connect(context.TODO())
-	assert.Error(t, err)
-}
-
-func TestConnector_Driver(t *testing.T) {
-	dbmock, _, _ := sqlmock.New()
-	db, err := Open("sqlmock_err", dbmock.Driver(), &bufferTestLogger{})
-	assert.NoError(t, err)
-	_, ok := interface{}(db).(*sql.DB)
-	assert.True(t, ok)
-	assert.Equal(t, dbmock.Driver(), db.Driver())
-	_ = dbmock.Close()
+func init() {
+	sql.Register("mock", &driverMock{})
 }
 
 func TestOpen(t *testing.T) {
-	dbmock, _, _ := sqlmock.New()
-	db, err := Open("sqlmock_db_0", dbmock.Driver(), &bufferTestLogger{})
-	assert.NoError(t, err)
-	_, ok := interface{}(db).(*sql.DB)
-	assert.True(t, ok)
-	_ = dbmock.Close()
+	t.Run("Without Options", func(t *testing.T) {
+		mockDriver := &driverMock{}
+		mockDriver.On("Open", mock.Anything).Return(&driverConnMock{}, nil)
+
+		db, err := Open("test", mockDriver, bufLogger)
+		assert.NoError(t, err)
+		_, ok := interface{}(db).(*sql.DB)
+		assert.True(t, ok)
+	})
+
+	t.Run("With Options", func(t *testing.T) {
+		mockDriver := &driverMock{}
+		mockDriver.On("Open", mock.Anything).Return(&driverConnMock{}, driver.ErrBadConn)
+
+		db, err := Open("test", mockDriver, bufLogger, WithErrorFieldname("errtest"), WithMinimumLevel(LevelDebug))
+		assert.NoError(t, err)
+		_, ok := interface{}(db).(*sql.DB)
+		assert.True(t, ok)
+		err = db.Ping()
+		assert.Error(t, err)
+
+		var output bufLog
+		err = json.Unmarshal(bufLogger.Bytes(), &output)
+		assert.NoError(t, err)
+		assert.Equal(t, "Connect", output.Message)
+		assert.Equal(t, LevelError.String(), output.Level)
+		assert.Contains(t, output.Data, "errtest")
+	})
 }
 
-func TestOpenWithOptions(t *testing.T) {
-	dbmock, _, _ := sqlmock.New()
-	db, err := Open("sqlmock_db_0", dbmock.Driver(), &bufferTestLogger{}, WithErrorFieldname("errtest"), WithMinimumLevel(LevelNotice))
-	assert.NoError(t, err)
-	_, ok := interface{}(db).(*sql.DB)
-	assert.True(t, ok)
-	_ = dbmock.Close()
+func TestConnector_Connect(t *testing.T) {
+	t.Run("Connect Error", func(t *testing.T) {
+		mockDriver := &driverMock{}
+		mockDriver.On("Open", mock.Anything).Return(&driverConnMock{}, driver.ErrBadConn)
+
+		con := &connector{dsn: "test", driver: mockDriver, logger: testLogger}
+		_, err := con.Connect(context.TODO())
+		assert.Error(t, err)
+
+		var output bufLog
+		err = json.Unmarshal(bufLogger.Bytes(), &output)
+		assert.NoError(t, err)
+		assert.Equal(t, "Connect", output.Message)
+		assert.Equal(t, LevelError.String(), output.Level)
+	})
+
+	t.Run("Connect Success", func(t *testing.T) {
+		mockDriver := &driverMock{}
+		mockDriver.On("Open", mock.Anything).Return(&driverConnMock{}, nil)
+
+		con := &connector{dsn: "test", driver: mockDriver, logger: testLogger}
+		_, err := con.Connect(context.TODO())
+		assert.NoError(t, err)
+
+		var output bufLog
+		err = json.Unmarshal(bufLogger.Bytes(), &output)
+		assert.NoError(t, err)
+		assert.Equal(t, "Connect", output.Message)
+		assert.Equal(t, LevelDebug.String(), output.Level)
+	})
+}
+
+func TestConnector_Driver(t *testing.T) {
+	mockDriver := &driverMock{}
+	con := &connector{dsn: "test", driver: mockDriver, logger: testLogger}
+	drv := con.Driver()
+
+	assert.Equal(t, mockDriver, drv)
+}
+
+type driverMock struct {
+	mock.Mock
+}
+
+func (m *driverMock) Open(name string) (driver.Conn, error) {
+	arg := m.Called(name)
+
+	return arg.Get(0).(driver.Conn), arg.Error(1)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/simukti/sqldb-logger
 go 1.13
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.3.3
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/rs/zerolog v1.16.0
 	github.com/stretchr/testify v1.4.0
