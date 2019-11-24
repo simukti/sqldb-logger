53cf75a6265479bd7a9b3ed46f5230da705cd621
Sarjono Mukti Aji <me@simukti.net>
2019-11-24 23:26:10 +0400

    add initial rows test

--- rows_test.go
+++ rows_test.go
@@ -4,10 +4,163 @@ package sqldblogger
 
 import (
 	"database/sql/driver"
+	"encoding/json"
+	"io"
 	"reflect"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
+
+func TestRows_Columns(t *testing.T) {
+	rowsMock := &rowsMock{}
+	rowsMock.On("Columns").Return([]string{"a", "b"})
+	rs := &rows{Rows: rowsMock, logger: testLogger}
+
+	cols := rs.Columns()
+	assert.Implements(t, (*driver.Rows)(nil), rs)
+	assert.Equal(t, cols, []string{"a", "b"})
+}
+
+func TestRows_Close(t *testing.T) {
+	t.Run("Error", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rowsMock.On("Close").Return(driver.ErrBadConn)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.Close()
+		assert.Implements(t, (*driver.Rows)(nil), rs)
+		assert.Error(t, err)
+
+		var output bufLog
+		err = json.Unmarshal(bufLogger.Bytes(), &output)
+		assert.NoError(t, err)
+		assert.Equal(t, "RowsClose", output.Message)
+		assert.Equal(t, LevelError.String(), output.Level)
+		bufLogger.Reset()
+	})
+
+	t.Run("Success", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rowsMock.On("Close").Return(nil)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.Close()
+		assert.Implements(t, (*driver.Rows)(nil), rs)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRows_Next(t *testing.T) {
+	t.Run("Error io.EOF", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rowsMock.On("Next", mock.Anything).Return(io.EOF)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.Next([]driver.Value{1})
+		assert.Implements(t, (*driver.Rows)(nil), rs)
+		assert.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+	})
+
+	t.Run("Error Non-io.EOF", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rowsMock.On("Next", mock.Anything).Return(driver.ErrBadConn)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.Next([]driver.Value{1})
+		assert.Implements(t, (*driver.Rows)(nil), rs)
+		assert.Error(t, err)
+
+		var output bufLog
+		err = json.Unmarshal(bufLogger.Bytes(), &output)
+		assert.NoError(t, err)
+		assert.Equal(t, "RowsNext", output.Message)
+		assert.Equal(t, LevelError.String(), output.Level)
+		bufLogger.Reset()
+	})
+
+	t.Run("Success", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rowsMock.On("Next", mock.Anything).Return(nil)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.Next([]driver.Value{1})
+		assert.Implements(t, (*driver.Rows)(nil), rs)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRows_HasNextResultSet(t *testing.T) {
+	t.Run("Non driver.RowsNextResultSet", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		flag := rs.HasNextResultSet()
+		assert.Equal(t, false, flag)
+	})
+
+	t.Run("driver.RowsNextResultSet", func(t *testing.T) {
+		rowsMock := &rowsRowsNextResultSetMock{}
+		rowsMock.On("HasNextResultSet").Return(true)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		flag := rs.HasNextResultSet()
+		assert.Equal(t, true, flag)
+	})
+}
+
+func TestRows_NextResultSet(t *testing.T) {
+	t.Run("Non driver.RowsNextResultSet", func(t *testing.T) {
+		rowsMock := &rowsMock{}
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.NextResultSet()
+		assert.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+	})
+
+	t.Run("Error io.EOF", func(t *testing.T) {
+		rowsMock := &rowsRowsNextResultSetMock{}
+		rowsMock.On("NextResultSet").Return(io.EOF)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.NextResultSet()
+		assert.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+		assert.Empty(t, bufLogger.Bytes())
+		bufLogger.Reset()
+	})
+
+	t.Run("Not Error", func(t *testing.T) {
+		rowsMock := &rowsRowsNextResultSetMock{}
+		rowsMock.On("NextResultSet").Return(nil)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.NextResultSet()
+		assert.NoError(t, err)
+		assert.Empty(t, bufLogger.Bytes())
+		bufLogger.Reset()
+	})
+
+	t.Run("Error Non io.EOF", func(t *testing.T) {
+		rowsMock := &rowsRowsNextResultSetMock{}
+		rowsMock.On("NextResultSet").Return(driver.ErrBadConn)
+		rs := &rows{Rows: rowsMock, logger: testLogger}
+
+		err := rs.NextResultSet()
+		assert.Error(t, err)
+		assert.NotEqual(t, io.EOF, err)
+
+		var output bufLog
+		err = json.Unmarshal(bufLogger.Bytes(), &output)
+		assert.NoError(t, err)
+		assert.Equal(t, "RowsNextResultSet", output.Message)
+		assert.Equal(t, LevelError.String(), output.Level)
+		bufLogger.Reset()
+	})
+}
 
 type rowsMock struct {
 	mock.Mock
