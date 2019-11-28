54f29ee5890638db6d8822317ec7911056066b3d
Sarjono Mukti Aji <me@simukti.net>
2019-11-28 21:44:21 +0400

    embed driver.Tx

--- transaction.go
+++ transaction.go
@@ -7,32 +7,34 @@ import (
 )
 
 type transaction struct {
-	tx     driver.Tx
+	driver.Tx
 	logger *logger
 }
 
-func (t *transaction) Commit() error {
+// Commit implement driver.Tx
+func (tx *transaction) Commit() error {
 	lvl, start := LevelDebug, time.Now()
-	err := t.tx.Commit()
+	err := tx.Tx.Commit()
 
 	if err != nil {
 		lvl = LevelError
 	}
 
-	t.logger.log(context.Background(), lvl, "Commit", start, err)
+	tx.logger.log(context.Background(), lvl, "Commit", start, err)
 
 	return err
 }
 
-func (t *transaction) Rollback() error {
+// Rollback implement driver.Tx
+func (tx *transaction) Rollback() error {
 	lvl, start := LevelDebug, time.Now()
-	err := t.tx.Rollback()
+	err := tx.Tx.Rollback()
 
 	if err != nil {
 		lvl = LevelError
 	}
 
-	t.logger.log(context.Background(), lvl, "Rollback", start, err)
+	tx.logger.log(context.Background(), lvl, "Rollback", start, err)
 
 	return err
 }
