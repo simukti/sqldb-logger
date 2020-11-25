d28c15787aa7662ee114571c8348bdda2aa3fae6
Sarjono Mukti Aji <me@simukti.net>
2020-11-25 21:08:47 +0700

    add note on why I need sed

--- test.sh
+++ test.sh
@@ -8,4 +8,5 @@ ls -d logadapter/* | xargs -I {} bash -c "cd '{}' \
 && go test -race -coverprofile=coverage.out -covermode=atomic -coverpkg=./... ./... \
 && cat coverage.out | grep -v \"mode:\" >> ../../coverage.out \
 && rm coverage.out"
+# for go repo with nested modules, remove repo prefix, otherwise goveralls will failed.
 sed -i -e 's/github.com\/simukti\/sqldb-logger/./g' coverage.out
