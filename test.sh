e17bbb78991201f2676886ff2311c1d4d4284332
Sarjono Mukti Aji <me@simukti.net>
2020-11-25 17:07:49 +0700

    move tests to test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -ev
+go vet
+golangci-lint run
+test -z "$(go fmt ./...)" # fail if not formatted properly
+go test -v -race -coverprofile=coverage.out -covermode=atomic ./...
+ls -d logadapter/* | xargs -I {} bash -c "cd '{}' \
+&& go test -race -coverprofile=coverage.out -covermode=atomic -coverpkg=./... ./... \
+&& cat coverage.out | grep -v \"mode:\" >> ../../coverage.out \
+&& rm coverage.out"
