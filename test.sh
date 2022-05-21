7807b1d940043ec63f9642034aa54135c1707233
Sarjono Mukti Aji <me@simukti.net>
2022-05-21 23:16:43 +0700

    use github action

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 go vet
-golangci-lint run
 test -z "$(go fmt ./...)" # fail if not formatted properly
 go test -v -race -coverprofile=coverage.out -covermode=atomic ./...
 ls -d logadapter/* | xargs -I {} bash -c "cd '{}' \
@@ -10,4 +9,3 @@ ls -d logadapter/* | xargs -I {} bash -c "cd '{}' \
 && rm coverage.out"
 # for go repo with nested modules, remove repo prefix, otherwise goveralls will failed.
 sed -i -e 's/github.com\/simukti\/sqldb-logger/./g' coverage.out
-sonar-scanner
